367b1c3a4b8a3b2edf9b9ad05b4d8c0a41f6530e
Hazezew <Hazezew@KFT-TECH-18.kifiya.et>
2019-06-17 11:45:24 +0300

    Handling hover and duplicate web elements

---
+++ DigitalBankTest/src/TestCases/TestMenu.java
@@ -0,0 +1,49 @@
+package TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestMenu {
+	WebDriver driver;
+	
+	@BeforeClass
+	public void start() {
+		System.setProperty("webdriver.chrome.driver", "D:\\GCS\\Selenium Webdrivers\\Version 74\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	@Test //handling hover over menu 
+	public void checkMenu() throws InterruptedException {		
+		driver.get("https://www.stima-sacco.com/");		
+		WebElement menuNews=driver.findElement(By.linkText("News"));
+		Actions hover=new Actions(driver);
+		hover.moveToElement(menuNews).perform();
+		
+		Thread.sleep(3000);
+		driver.findElement(By.linkText("Gallery")).click();
+		Assert.assertEquals(driver.getCurrentUrl(), "https://www.stima-sacco.com/index.php/news/gallery");		
+	}
+	@Test //Handling duplicate web element
+	public void checkInfiBean() throws InterruptedException {
+		driver.navigate().to("https://www.infibeam.com/MyAccount.action");
+		driver.findElement(By.id("new-account-btn")).click();
+		
+		driver.findElement(By.id("password")).sendKeys("mypassword");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//form[@name='registration-form']")).findElement(By.id("password")).sendKeys("yourpassword");
+		Thread.sleep(2000);		
+	}
+	
+	@AfterClass
+	public void close() {
+		driver.close();
+	}
+}
